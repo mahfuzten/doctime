fae1cccebacdf626f9c31e4e29929a8603c92a91
bullgit <bullgit@gmail.com>
2022-07-16 13:41:33 +0600

    patients and doctors pages add

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,4 +11,25 @@ class FrontendController extends Controller
     {
         return view('frontend.home');
     }
+    
+    public function showLoginPage()
+    {
+        return view('frontend.login');
+    }
+    public function showPatientRegisterPage()
+    {
+        return view('frontend.patient.register');
+    }
+    public function showPatientDashPage()
+    {
+        return view('frontend.patient.dashboard');
+    }
+    public function showDoctorRegisterPage()
+    {
+        return view('frontend.doctor.register');
+    }
+    public function showDoctorDashPage()
+    {
+        return view('frontend.doctor.dashboard');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[ FrontendController::class,'showHomePage' ])-> name('home.page');
+Route::get('/login',[ FrontendController::class,'showLoginPage' ])-> name('login.page');
+
+Route::get('/patient-register',[ FrontendController::class,'showPatientRegisterPage' ])-> name('patient.reg.page');
+Route::get('/patient-dashboard',[ FrontendController::class,'showPatientDashPage' ])-> name('patient.dash.page');
+
+Route::get('/doctor-register',[ FrontendController::class,'showDoctorRegisterPage' ])-> name('doctor.reg.page');
+Route::get('/doctor-dashboard',[ FrontendController::class,'showDoctorDashPage' ])-> name('doctor.dash.page');
+
